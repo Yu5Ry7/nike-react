45d991f643e990c9ea00434933f06cbc3dc1e743
Yu5Ry7 <startupinit2@gmai.com>
2023-08-30 08:23:25 -0700

    done and dusted

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,8 +1,38 @@
-
+import ReviewCard from "../assets/Components/ReviewCard"
+import { reviews } from "../constants"
 
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="
+      max-container
+      ">
+    <h3 className="font-palanquin text-center text-4xl font-bold">what Our
+      <span className="text-coral-red"> Cutomers</span> say?
+    </h3>
+    <p className="info-text
+    m-auto
+    mt-4
+    max-w-lg
+    text-center">Hear genuine stories from our satisfied customers about their exceptional experiences with us.</p>
+
+    <div className="mt-24
+    flex
+    flex-1
+    justify-evenly
+    items-center
+    gap-14
+    max-lg:flex-col">
+        {reviews.map((review) =>(
+          <ReviewCard 
+          key={review.customerName}
+          imgURL={review.imgURL}
+          customerName={review.customerName}
+          rating={review.rating}
+          feedback={review.feedback}
+          />
+        ) )}
+    </div>
+    </section>
   )
 }
 
